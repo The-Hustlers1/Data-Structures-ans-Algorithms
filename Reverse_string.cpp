270ce0118a158f3d4a184c4e02dd6776b49221ba
Ananya Vishnoi <71982267+ananya26-vishnoi@users.noreply.github.com>
2021-07-09 13:32:40 +0530

    Update Reverse_string.cpp

--- Reverse_string.cpp
+++ Reverse_string.cpp
@@ -1,5 +1,3 @@
-//*1st method
-
 #include <bits/stdc++.h>
 using namespace std;
 void reversestr(string str)
@@ -8,43 +6,19 @@ void reversestr(string str)
 
     for (int i = 0; i < n / 2; i++)
     {
-        swap(str[i], str[n - i - 1]);
+        char temp=str.at(i);
+        str.at(i)=str.at(str.length()-1-i);
+        str.at(str.length()-1-i)=temp;
     }
+    for (int i = 0; i < n ; i++){
+        cout<<str.at(i);
+    }
+    
 }
 int main()
 {
     string str;
     getline(cin, str);
     reversestr(str);
-    cout << str;
-    return 0;
-}
-
-//* 2nd method
-
-#include <bits/stdc++.h>
-using namespace std;
-
-void rev_string(string str)
-{
-    for (int i = 0; i < str.length() / 2; i++)
-    {
-        char temp = str.at(i);
-        str.at(i) = str.at(str.length() - 1 - i);
-        str.at(str.length() - 1 - i) = temp;
-    }
-    for (int i = 0; i < str.length(); i++)
-    {
-        cout << str.at(i) << " ";
-    }
-}
-
-int main()
-{
-    string s;
-    cin >> s;
-    rev_string(s);
-
-    cout << endl;
     return 0;
 }
